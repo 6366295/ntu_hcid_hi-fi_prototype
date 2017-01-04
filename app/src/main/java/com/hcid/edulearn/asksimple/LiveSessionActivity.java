d4e566310b6ca7982b319aacf6c3f02232f619b5
Peng-Hsuan Li <jacobvsdanniel@gmail.com>
2017-01-04 13:21:05 +0800

    session overview and in-session

--- app/src/main/java/com/hcid/edulearn/asksimple/LiveSessionActivity.java
+++ app/src/main/java/com/hcid/edulearn/asksimple/LiveSessionActivity.java
@@ -1,5 +1,6 @@
 package com.hcid.edulearn.asksimple;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -22,6 +23,23 @@ public class LiveSessionActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             findViewById(R.id.toolbar_shadow).setVisibility(View.GONE);
         }
+
+        // set texts
+        TextView tv1 = (TextView) findViewById(R.id.textView1);
+        tv1.setText("29");
+        tv1.setTextColor(Color.BLACK);
+
+        TextView tv2 = (TextView) findViewById(R.id.textView2);
+        tv2.setText("16");
+        tv2.setTextColor(Color.RED);
+
+        TextView tv3 = (TextView) findViewById(R.id.textView3);
+        tv3.setText("13");
+        tv3.setTextColor(Color.BLACK);
+
+        TextView tv4 = (TextView) findViewById(R.id.textView4);
+        tv4.setText("10");
+        tv4.setTextColor(Color.BLACK);
     }
 
     public void ask_question(View view) {
@@ -36,47 +54,55 @@ public class LiveSessionActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.textView1);
         ImageView iv = (ImageView) findViewById(R.id.imageView1);
         if(tv.getText() == "29"){
-            iv.setImageResource(R.drawable.like);
+            iv.setImageResource(R.drawable.like_ab);
             tv.setText("30");
+            tv.setTextColor(Color.RED);
         }else{
-            iv.setImageResource(R.drawable.not_like);
+            iv.setImageResource(R.drawable.like_aa);
             tv.setText("29");
+            tv.setTextColor(Color.BLACK);
         }
     }
 
     public void click_2(View view) {
         TextView tv = (TextView) findViewById(R.id.textView2);
         ImageView iv = (ImageView) findViewById(R.id.imageView2);
-        if(tv.getText() == "3"){
-            iv.setImageResource(R.drawable.like);
-            tv.setText("4");
+        if(tv.getText() == "15"){
+            iv.setImageResource(R.drawable.like_ab);
+            tv.setText("16");
+            tv.setTextColor(Color.RED);
         }else{
-            iv.setImageResource(R.drawable.not_like);
-            tv.setText("3");
+            iv.setImageResource(R.drawable.like_aa);
+            tv.setText("15");
+            tv.setTextColor(Color.BLACK);
         }
     }
 
     public void click_3(View view) {
         TextView tv = (TextView) findViewById(R.id.textView3);
         ImageView iv = (ImageView) findViewById(R.id.imageView3);
-        if(tv.getText() == "16"){
-            iv.setImageResource(R.drawable.like);
-            tv.setText("17");
+        if(tv.getText() == "13"){
+            iv.setImageResource(R.drawable.like_ab);
+            tv.setText("14");
+            tv.setTextColor(Color.RED);
         }else{
-            iv.setImageResource(R.drawable.not_like);
-            tv.setText("16");
+            iv.setImageResource(R.drawable.like_aa);
+            tv.setText("13");
+            tv.setTextColor(Color.BLACK);
         }
     }
 
     public void click_4(View view) {
         TextView tv = (TextView) findViewById(R.id.textView4);
         ImageView iv = (ImageView) findViewById(R.id.imageView4);
-        if(tv.getText() == "0"){
-            iv.setImageResource(R.drawable.like);
-            tv.setText("1");
+        if(tv.getText() == "10"){
+            iv.setImageResource(R.drawable.like_ab);
+            tv.setText("11");
+            tv.setTextColor(Color.RED);
         }else{
-            iv.setImageResource(R.drawable.not_like);
-            tv.setText("0");
+            iv.setImageResource(R.drawable.like_aa);
+            tv.setText("10");
+            tv.setTextColor(Color.BLACK);
         }
     }
 }
