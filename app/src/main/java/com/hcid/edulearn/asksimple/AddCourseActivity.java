1590a2e47fe6a3158bfb1a9ca139e97b86e14194
Mike Trieu <mt_tr1pl@hotmail.com>
2017-01-04 23:34:17 +0800

    Added QR scanner

--- app/src/main/java/com/hcid/edulearn/asksimple/AddCourseActivity.java
+++ app/src/main/java/com/hcid/edulearn/asksimple/AddCourseActivity.java
@@ -2,12 +2,20 @@ package com.hcid.edulearn.asksimple;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class AddCourseActivity extends AppCompatActivity {
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class AddCourseActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+
+    private ZBarScannerView mScannerView;
 
     Toolbar toolbar;
 
@@ -25,6 +33,10 @@ public class AddCourseActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
+        ViewGroup contentFrame = (ViewGroup) findViewById(R.id.camera_preview);
+        mScannerView = new ZBarScannerView(this);
+        contentFrame.addView(mScannerView);
+
     }
 
     // Source: http://stackoverflow.com/questions/26651602/display-back-arrow-on-toolbar-android
@@ -36,6 +48,36 @@ public class AddCourseActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Toast.makeText(this, "Contents = " + rawResult.getContents() +
+                ", Format = " + rawResult.getBarcodeFormat().getName(), Toast.LENGTH_SHORT).show();
+        // Note:
+        // * Wait 2 seconds to resume the preview.
+        // * On older devices continuously stopping and resuming camera preview can result in freezing the app.
+        // * I don't know why this is the case but I don't have the time to figure out.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mScannerView.resumeCameraPreview(AddCourseActivity.this);
+            }
+        }, 2000);
     }
 
     public void buttonAdd(View view) {
