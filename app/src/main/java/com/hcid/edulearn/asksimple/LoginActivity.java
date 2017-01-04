3c0d948acbc53d5855ab3f8d5b38f61cd9afddb2
Mike Trieu <mt_tr1pl@hotmail.com>
2017-01-04 21:58:28 +0800

    Refactored RegisteredActivity

--- app/src/main/java/com/hcid/edulearn/asksimple/LoginActivity.java
+++ app/src/main/java/com/hcid/edulearn/asksimple/LoginActivity.java
@@ -1,11 +1,11 @@
 package com.hcid.edulearn.asksimple;
 
-import android.support.v7.app.AppCompatActivity;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
